e997c62c3e4e0b7e415048bb69b5909b2ea8a108
s0Fi98 <solmansofihussain@gmail.com>
2026-02-13 04:28:19 +0530

    initial commit after fixing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import { Button } from "antd";
 
 function App() {
 
   return (
     <React.Fragment>
-
+      <div style={{ padding: "40px" }}>
+        <h1>HRMS Dashboard</h1>
+        <Button type="primary">Ant Design Working 🚀</Button>
+      </div>
     </React.Fragment>
   )
 }
