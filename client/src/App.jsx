c618d8f2a531d0f5fc6291bb8c1b55fc75708cb4
s0Fi98 <solmansofihussain@gmail.com>
2026-02-13 03:23:05 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <React.Fragment>
-
+      This is the client side of the application. Please run the server to see the full functionality.
     </React.Fragment>
   )
 }
